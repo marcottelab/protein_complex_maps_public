cbd6cf0a715ae305b85d956dea2e34e88a435aed
Kevin Drew <kdrew@utexas.edu>
2016-07-21 15:48:18 -0500

    Updating public repository with changes made in master.
    Just grabbed everything in complexes and features and added a few things
    in plots and util

--- protein_complex_maps/util/pairwise2clusterid.py
+++ protein_complex_maps/util/pairwise2clusterid.py
@@ -2,6 +2,7 @@
 
 import argparse
 import itertools as it
+import pandas as pd
 
 def main():
 
@@ -10,6 +11,18 @@ def main():
                                             help="Filename of pairwise file, (ie. one line per ppi, third column contains score)")
     parser.add_argument("--cluster_filename", action="store", dest="cluster_filename", required=True,
                                             help="Filename of cluster file, (ie. one line per cluster)")
+    parser.add_argument("--input_feature_matrix", action="store", dest="feature_matrix", required=False, default=None, 
+                                    help="Filename of input feature matrix")
+    parser.add_argument("--sep", action="store", dest="sep", required=False, default=',',
+                                    help="Column separator for input feature matrix file, default=,")
+    parser.add_argument("--header_names", action="store", nargs='+', dest="header_names", required=False, default=None,
+                                    help="Names for header relating to features")
+    parser.add_argument("--features", action="store", nargs='+', dest="features", required=False, default=None,
+                                    help="Names of fields for features in input_feature_matrix, will test !=0.0 unless --write_value set")
+    parser.add_argument("--write_value", action="store_true", dest="write_value", required=False, default=False,
+                                    help="Output values for features in input_feature_matrix specified by --features")
+    parser.add_argument("--id_columns", action="store", nargs='+', dest="id_columns", required=False, default=None,
+                                    help="Names of columns for ids in input_feature_matrix")
     parser.add_argument("--output_filename", action="store", dest="out_filename", required=True,
                                             help="Output filename ")
     args = parser.parse_args()
@@ -27,9 +40,26 @@ def main():
         except IndexError:
             continue
 
+    print "read input files"
     fout = open(args.out_filename,"wb")
 
-    fout.write("id1\tscore\n")
+    if args.feature_matrix != None:
+        feature_table = pd.read_csv(args.feature_matrix,sep=args.sep)
+        if 'frozenset_ids_str_order' not in feature_table.columns:
+            #kdrew: create frozenset_ids_str_order column
+            feature_table['frozenset_ids'] = map(frozenset,feature_table[[args.id_columns[0],args.id_columns[1]]].values)
+            feature_table['frozenset_ids_str_order'] = feature_table['frozenset_ids'].apply(list).apply(sorted).apply(str)
+            feature_table = feature_table.set_index(['frozenset_ids_str_order'])
+
+        if args.header_names != None:
+            fout.write("id1\tscore\t%s\n" % "\t".join(args.header_names))
+        else:
+            fout.write("id1\tscore\t%s\n" % "\t".join(args.features))
+    else:
+        fout.write("id1\tscore\n")
+
+    print "Wrote Header"
+
     for clustid, cluster in enumerate(clusters):
         for prot_pair in it.combinations(cluster,2):
             id1 = "%s_%s" % (clustid, prot_pair[0])
@@ -38,7 +68,21 @@ def main():
                 score = pairwise_interactions[frozenset([prot_pair[0],prot_pair[1]])]
             except KeyError:
                 continue
-            fout.write("%s (pp) %s\t%s" % (id1, id2, score))
+            if args.feature_matrix != None:
+                out_list = []
+                ids_str_order = "%s" % sorted([prot_pair[0],prot_pair[1]])
+                for field in args.features:
+                    if args.write_value:
+                        #out_list.append(str(feature_table[feature_table['frozenset_ids_str_order'] == ids_str_order][field].values[0]))
+                        out_list.append(str( feature_table.loc[ids_str_order][field] ) )
+                    else:
+                        #kdrew: seems inefficent 
+                        #out_list.append(str(feature_table[feature_table['frozenset_ids_str_order'] == ids_str_order][field].values[0] != 0.0))
+                        out_list.append(str( feature_table.loc[ids_str_order][field] != 0.0))
+
+                fout.write("%s (pp) %s\t%s\t%s" % (id1, id2, score, "\t".join(out_list)))
+            else:
+                fout.write("%s (pp) %s\t%s" % (id1, id2, score))
             fout.write("\n")
 
     fout.close()
